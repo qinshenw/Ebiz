63254acceb45f6e3c97f3859bd14451e6a8f6df2
yvainePancmu <56243512+yvainePancmu@users.noreply.github.com>
2022-07-28 16:18:26 +0800

    Add files via upload

--- vue-element/src/pages/admin/notice/dataSetList.vue
+++ vue-element/src/pages/admin/notice/dataSetList.vue
@@ -52,9 +52,24 @@ export default {
         }
       })
     },
+    funDownload (content, filename) {
+      // 创建隐藏的可下载链接
+      var eleLink = document.createElement('a');
+      eleLink.download = filename + '_report';
+      eleLink.style.display = 'none';
+      // 字符内容转变成blob地址
+      var blob = new Blob([content]);
+      eleLink.href = URL.createObjectURL(blob);
+      // 触发点击
+      document.body.appendChild(eleLink);
+      eleLink.click();
+      // 然后移除
+      document.body.removeChild(eleLink);
+    },
     export_report(item) {
       get(`/api/report/export?username=${this.user.name}&filename=${item.filename}`).then((res) => {
         console.log("res = ", res)
+        this.funDownload(res, item.filename.split('.')[0])
       });
     }
   },
